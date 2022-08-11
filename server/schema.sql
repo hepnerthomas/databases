957706ea782077d349f7ba6d2d5cfb0d2a06abc2
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-11 10:23:31 -0700

    feat: add multi-table schema design.

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,37 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE Users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_name VARCHAR(20)
+);
 
+CREATE TABLE Rooms (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  room_name VARCHAR(20)
 
+);
+
+CREATE TABLE Messages (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  text VARCHAR(280),
+  user_id INTEGER,
+  room_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES Users(id),
+  FOREIGN KEY (room_id) REFERENCES Rooms(id)
+);
+
+CREATE TABLE Friends (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  user_id INTEGER,
+  friend_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES Users(id),
+  FOREIGN KEY (friend_id) REFERENCES Users(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
